0e6156e9613ebb15a9b3814319d172b160cedb34
Prak Sreypov <sreypovprak88@gmail.com>
2024-06-12 11:13:47 +0700

    change path in homepage-data.js and footer

--- assets/js/data/homepage-data.js
+++ assets/js/data/homepage-data.js
@@ -1,42 +1,42 @@
 export const HeroBanner = [
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/Lucas.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/Lucas.webp",
         titleB1: "Watch Video",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/badminton-cover.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/badminton-cover.webp",
         titleB1: "Learn More",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/badminton-image.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/badminton-image.webp",
         titleB1: "Read More",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/beyond-i-see.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/beyond-i-see.webp",
         titleB1: "Watch Video",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/bwis_manifest_1002.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/bwis_manifest_1002.webp",
         titleB1: "Read More",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/ezong.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/ezong.webp",
         titleB1: "Learn More",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/fly-direction.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/fly-direction.webp",
         titleB1: "Read More",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/made-by-yonex-desktop.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/made-by-yonex-desktop.webp",
         titleB1: "Learn More",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/new-dawn.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/new-dawn.webp",
         titleB1: "Learn More",
     },
     {
-        img: "/tech-a-project/2nd-mission/assets/images/homePage-hero/shoes.webp",
+        img: "/2nd-mission/assets/images/homePage-hero/shoes.webp",
         titleB1: "Read More",
     },
 ];
@@ -44,79 +44,79 @@ export const HeroBanner = [
 export const HeroRepsonsive = [
     {
         imgResponsive: [
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/lucas.webp",
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/badminton-1000.webp",
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/beyond-what-i-see.webp",
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/craft.webp",
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/counter-drive.webp",
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/craft.webp",
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/cover.webp",
-        "/tech-a-project/2nd-mission/assets/images/homePage-hero/responsive/far-beyond.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/lucas.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/badminton-1000.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/beyond-what-i-see.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/craft.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/counter-drive.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/craft.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/cover.webp",
+        "/2nd-mission/assets/images/homePage-hero/responsive/far-beyond.webp",
         ],
     },
 ];
 
 export const CardSlider = [
     {
-        link: "/tech-a-project/2nd-mission/public/product-review-badminton.html",
-        img: "/tech-a-project/2nd-mission/assets/images/sport-image/badminton.webp",
+        link: "/2nd-mission/public/product-review-badminton.html",
+        img: "/2nd-mission/assets/images/sport-image/badminton.webp",
         title: "BADMINTON",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/product-review-golf.html",
-        img: "/tech-a-project/2nd-mission/assets/images/sport-image/golf.webp",
+        link: "/2nd-mission/public/product-review-golf.html",
+        img: "/2nd-mission/assets/images/sport-image/golf.webp",
         title: "GOLF",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/product-review-all-running.html",
-        img: "/tech-a-project/2nd-mission/assets/images/sport-image/orange-shoes.webp",
+        link: "/2nd-mission/public/product-review-all-running.html",
+        img: "/2nd-mission/assets/images/sport-image/orange-shoes.webp",
         title: "RUNNING",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/product-review-tennis.html",
-        img: "/tech-a-project/2nd-mission/assets/images/sport-image/tennis.webp",
+        link: "/2nd-mission/public/product-review-tennis.html",
+        img: "/2nd-mission/assets/images/sport-image/tennis.webp",
         title: "TENNIS",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/product-review-snowboarding.html",
-        img: "/tech-a-project/2nd-mission/assets/images/sport-image/snowboard.webp",
+        link: "/2nd-mission/public/product-review-snowboarding.html",
+        img: "/2nd-mission/assets/images/sport-image/snowboard.webp",
         title: "SNOWBOARD",
     },
 ];
 
 export const CardSliderNewArrival = [
     {
-        link: "/tech-a-project/2nd-mission/public/detail-golf.html",
-        img: "/tech-a-project/2nd-mission/assets/images/detail-golf/ezone.png",
+        link: "/2nd-mission/public/detail-golf.html",
+        img: "/2nd-mission/assets/images/detail-golf/ezone.png",
         title: "EZONE GT DRIVER TYPE S",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/detail-tennis.html",
-        img: "/tech-a-project/2nd-mission/assets/images/detail-tennis/percept97.png",
+        link: "/2nd-mission/public/detail-tennis.html",
+        img: "/2nd-mission/assets/images/detail-tennis/percept97.png",
         title: "PERCEPT 97",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/detail-badminton.html",
-        img: "/tech-a-project/2nd-mission/assets/images/productPage/Astrox88.png",
+        link: "/2nd-mission/public/detail-badminton.html",
+        img: "/2nd-mission/assets/images/productPage/Astrox88.png",
         title: "ASTROX 88 S GAME",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/detail-snowboard.html",
-        img: "/tech-a-project/2nd-mission/assets/images/detail-snowboard/regna.png",
+        link: "/2nd-mission/public/detail-snowboard.html",
+        img: "/2nd-mission/assets/images/detail-snowboard/regna.png",
         title: "REGNA",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/detail-women.html",
+        link: "/2nd-mission/public/detail-women.html",
         img: "https://www.yonex.com/media/catalog/product/s/h/shr100xl_663_1_1.png?quality=80&fit=bounds&height=819&width=600&canvas=600:819",
         title: "SAFERUN 100X WOMEN",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/detail-men.html",
-        img: "/tech-a-project/2nd-mission/assets/images/running-page/shr100xm_440_1.webp",
+        link: "/2nd-mission/public/detail-men.html",
+        img: "/2nd-mission/assets/images/running-page/shr100xm_440_1.webp",
         title: "SAFERUN 100X MEN",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/detail-unisex.html",
+        link: "/2nd-mission/public/detail-unisex.html",
         img: "https://www.yonex.com/media/catalog/product/s/h/shr900c_bk007_fs_rs_1.png?quality=80&fit=bounds&height=819&width=600&canvas=600:819",
         title: "SAFERUN 900C",
     },
@@ -124,38 +124,38 @@ export const CardSliderNewArrival = [
 
 export const CardSliderAthlete = [
     {
-        link: "/tech-a-project/2nd-mission/public/athlete-profile-badminton.html",
-        img: "/tech-a-project/2nd-mission/assets/images/product-review/al1.webp",
+        link: "/2nd-mission/public/athlete-profile-badminton.html",
+        img: "/2nd-mission/assets/images/product-review/al1.webp",
         title: "VIKTOR AXELSON",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/athlete-profile-golf.html",
+        link: "/2nd-mission/public/athlete-profile-golf.html",
         img: "https://www.yonex.com/media/athletes/tmp/square/kim_570x570_.jpg",
         title: "HYO JOO KIM",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/athlete-profile-tennis.html",
-        img: "/tech-a-project/2nd-mission/assets/images/product-review/t1.webp",
+        link: "/2nd-mission/public/athlete-profile-tennis.html",
+        img: "/2nd-mission/assets/images/product-review/t1.webp",
         title: "ELENA RYBAKINA",
     },
     {
-        link: "/tech-a-project/2nd-mission/public/athlete-profile-snowboarding.html",
+        link: "/2nd-mission/public/athlete-profile-snowboarding.html",
         img: "https://www.yonex.com/media/athletes/tmp/square/Yuto-square.jpg",
         title: "YUTO TOTSKA",
     },
     {
         link: "#",
-        img: "/tech-a-project/2nd-mission/assets/images/product-review/nakashima_570x570.webp",
+        img: "/2nd-mission/assets/images/product-review/nakashima_570x570.webp",
         title: " BRANDOM NAKASHIMA",
     },
     {
         link: "#",
-        img: "/tech-a-project/2nd-mission/assets/images/product-review/vekic_570x570.webp",
+        img: "/2nd-mission/assets/images/product-review/vekic_570x570.webp",
         title: "DONNA VEKIC",
     },
     {
         link: "#",
-        img: "/tech-a-project/2nd-mission/assets/images/product-review/kim_570x570_.webp",
+        img: "/2nd-mission/assets/images/product-review/kim_570x570_.webp",
         title: "HYOO JOO KIM",
     },
 ];
